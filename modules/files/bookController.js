a879ad93f994538fe3ce45f12199c1e77ffff257
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2021-08-12 16:44:45 +0300

    file upload structure is changed

--- modules/files/bookController.js
+++ modules/files/bookController.js
@@ -1,26 +1,14 @@
 const {fileUpload} = require('./queries')
 
-module.exports= async function (req, res){
+module.exports= function (req, res){
     const file = req.file
+    
     if(file){
-        try{
-            const row = await fileUpload(file.filename, file.mimetype, file.size)
-            if(row){
-                return{
-                    status:200,
-                    message:"Successfully created",
-                    data:row
-                }
-            }
-            throw new Error("record is not created you didn't fill required fields")
-        }
-        catch(err){
-            return{
-                status:400,
-                message:err.message,
-                data:null
-            }
-        }
-
+        const row = await fileUpload(file.filename, file.mimetype, file.size)
+        if(row){
+            res.send(row)     
+        }    
     }
+
+   
 }
--- modules/files/coverController.js
+++ modules/files/coverController.js
@@ -1,26 +1,13 @@
 const {coverUpload} = require('./queries.js')
 
-module.exports= async function (req, res){
+module.exports=  async function (req, res){
     const file = req.file
-    if(file){
-        try{           
-            const row = await coverUpload(file.filename, file.mimetype, file.size)           
-            if(row){
-                return{
-                    status:200,
-                    message:"Successfully created",
-                    data:row
-                }
-            }
-            throw new Error("record is not created you didn't fill required fields")
+   
+    if(file){            
+        const row = await coverUpload(file.filename, file.mimetype, file.size)           
+        if(row){
+            res.send(row)           
         }
-        catch(err){
-            return{
-                status:400,
-                message:err.message,
-                data:null
-            }
-        }
+    } 
 
-    }
 }
