ce584cb9a5ce389ca55946259e6fe8f4f18e5563
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2021-11-24 14:22:32 +0500

    database url

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT||5000;
 
 
-const connectionString = process.env.DATABASE_URL|| "postgres://postgres:123@localhost:5432/dba"
+const connectionString = process.env.DATABASE_URL|| "postgres://vldpdyhg:xJ6nACWbKQJ3n38ZZmhK_FJbYOCHYY2R@satao.db.elephantsql.com/vldpdyhg"
 module.exports ={
     PORT,
     connectionString
